0f47a058670fe97b9076fb0dc82e6fc65195f87c
Charliecwei <1538673717@qq.com>
2021-01-28 12:01:47 +0800

    修改数据结构，\phi转置测试函数转置

--- fealpystudy/hw.possion/hw2generalenlpic.py
+++ fealpystudy/hw.possion/hw2generalenlpic.py
@@ -18,13 +18,13 @@ n = int(sys.argv[2])
 
 x, y = sp.symbols("x0,x1")
 u = sp.sin(sp.pi*x)*sp.sin(sp.pi*y)
-#a = np.array([[10.0,-1.0], [-1.0,1.0]],dtype=np.float64)
-a = np.array([[0.0,0.0], [0.0,0.0]],dtype=np.float64)
+a = np.array([[10.0,-1.0], [-1.0,1.0]],dtype=np.float64)
+#a = np.array([[10.0,-1.0], [-1.0,2.0]],dtype=np.float64)
 #a = sp.Matrix([[x+100, y], [x, y+100]])
 b = np.array([1.0,1.0],dtype=np.float64)
 #b = sp.Matrix([sp.sin(x),sp.sin(y)])
-#c = 1+x**2+y**2
-c = np.array([0.0],dtype=np.float64)
+c = 1+x**2+y**2
+#c = np.array([1000.0],dtype=np.float64)
 
 
 pde = generalelliptic(u,x,y,a=a,b=b,c=c,Dirichletbd='(x==1.0)|(x==0.0)|(y==0.0)|(y==1.0)')
@@ -55,24 +55,17 @@ for i in range(4):
     As = pde.A(ps)
 
     if len(As.shape) == 2:
-        A = np.einsum('i, ijkl, nl, ijmn, j->jkm', ws, gphi, As, gphi, cellmeasure) #(NC, ldof, ldof)
+        A = np.einsum('i, ijkl, ln, ijmn, j->jkm', ws, gphi, As, gphi, cellmeasure) #(NC, ldof, ldof)
     else:
-        A = np.einsum('i, ijkl, ijnl, ijmn, j->jkm', ws, gphi, As, gphi, cellmeasure)
+        A = np.einsum('i, ijkl, ijln, ijmn, j->jkm', ws, gphi, As, gphi, cellmeasure)
     
     #construct matrix B
     phi = space.basis(bcs)# (NQ. NC, ldof)
     Bs = pde.B(ps) #(NQ, NC, GD)
     if len(Bs.shape) == 1:
-        B = -np.einsum('i, ijk, l, ijml, j->jkm', ws, phi, Bs, gphi, cellmeasure)
+        B = np.einsum('i, ijk, l, ijml, j->jkm', ws, phi, Bs, gphi, cellmeasure)
     else:
-        B = -np.einsum('i, ijk, ijl, ijml, j->jkm', ws, phi, Bs, gphi, cellmeasure)
-
-    #construct matrxi divb
-    Bs = pde.DivB(ps)  #(NQ, NC)
-    if len(Bs.shape) == 1:
-        DivB = -np.einsum('i, ijk, k, ijm, j->jkm', ws, phi, Bs, phi, cellmeasure)
-    else:
-        DivB = -np.einsum('i, ijk, ij, ijm, j->jkm', ws, phi, Bs, phi, cellmeasure)
+        B = np.einsum('i, ijk, ijl, ijml, j->jkm', ws, phi, Bs, gphi, cellmeasure)
 
     
     #construct matrix C
@@ -85,7 +78,7 @@ for i in range(4):
     
     #print(C[1:5,...])
     #construct stiff matrix A
-    A = A + B + DivB + C
+    A = A + B + C
     gdof = space.number_of_global_dofs()
     cell2dof = space.cell_to_dof()
     I = np.broadcast_to(cell2dof[:,:, None],shape=A.shape)
@@ -121,7 +114,7 @@ for i in range(4):
 
 
 k=0
-#showmultirate(plt, k, NDof, errormatrix, errortype, propsize=20)
+showmultirate(plt, k, NDof, errormatrix, errortype, propsize=20)
 show_error_table(NDof, errortype,  errormatrix)
 
 #plt.show()
