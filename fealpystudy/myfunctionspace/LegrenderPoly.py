96757e969b99fcfa4d3a987f501b9eeb809a917c
MathChen <1538673717@qq.com>
2021-02-21 15:17:03 +0800

    Legrendre

--- fealpystudy/myfunctionspace/LegrenderPoly.py
+++ fealpystudy/myfunctionspace/LegrenderPoly.py
@@ -3,6 +3,10 @@ import sympy as sp
 
 
 class Legrendrepoly:
+    '''
+        d维单纯型上的n次Legrendre多项式
+        Legrendre polynomials of degree n on d-dimensional simple form
+    '''
     def __init__(self,d=None,n=None):
         if d is None:
             d = 1
@@ -13,10 +17,13 @@ class Legrendrepoly:
         self.d = d
         self.n = n
         self.beta = self.multindex(d=None,n=None)
-        self.Legrendrepolynomial(beta=None)
+        self.L_beta = self.Legrendrepolynomial(beta=None)
 
     def beta(self):
         return self.beta
+
+    def L_beta(self):
+        return self.L_beta
 
     def Legrendrepolynomial(self,beta=None):
         if beta is None:
@@ -24,13 +31,62 @@ class Legrendrepoly:
 
         shape = np.shape(beta)
         d = shape[-1]
-        shape = shape[:-1]
-       # print(shape)
-        lam = sp.symbols('la:%d'%d)
-        
+        shape = np.array(shape)
+        shape[-1] = d+1
+        L = np.shape(shape)[0]-1
+        betas = np.zeros(shape,dtype=int)
+        betas[...,1:] = beta
+        beta = betas
+        lam = np.array(sp.symbols('la:%d'%(d+1)))
+       # Lam_beta = np.broadcast_to(np.expand_dims(lam,axis=np.arange(L)),shape)
 
-        return lam
-        
+       # print(Lam_beta**beta)
+       # print(beta)
+        vert_underline_beta = self.vert_underline_beta(beta)
+        vert_overline_lambda = self.vert_overline_lambda(lam)
+
+        derivatives_multindex = np.zeros(vert_underline_beta.shape,dtype=int)
+        derivatives_multindex[...,0] = vert_underline_beta[...,0]
+        for i in range(1,d+1):
+            derivatives_multindex[...,i] = vert_underline_beta[...,i]+vert_underline_beta[...,i-1]+i-1
+
+        Lam_beta = np.broadcast_to(np.expand_dims(lam,axis=np.arange(L)),shape)**derivatives_multindex\
+                        *np.broadcast_to(np.expand_dims(1-vert_overline_lambda,axis=np.arange(L)),shape)**beta #shape=(...,d+1)
+
+        lam = np.broadcast_to(np.expand_dims(lam,axis=np.arange(L)),shape)
+
+        #print(Lam_beta[-1,...],derivatives_multindex[-1,...])
+        lam = lam.reshape(-1)
+        Lam_beta = Lam_beta.reshape(-1)
+        derivatives_multindex = derivatives_multindex.reshape(-1)
+
+        for i in range(len(lam)):
+            Lam_beta[i] = sp.diff(Lam_beta[i],lam[i],derivatives_multindex[i])/np.math.factorial(derivatives_multindex[i])
+
+        Lam_beta = Lam_beta.reshape(shape)
+        Lam_beta = np.prod(Lam_beta,axis=-1)
+
+        return Lam_beta
+
+
+    def vert_underline_beta(self,beta):
+        vert_underline_beta = np.zeros(beta.shape,dtype=int)
+        vert_underline_beta[...,0] = beta[...,0]
+        D = np.shape(vert_underline_beta)[-1]
+        for i in range(1,D):
+            vert_underline_beta[...,i] = beta[...,i]+vert_underline_beta[...,i-1]
+        return vert_underline_beta
+
+
+    def vert_overline_lambda(self,lam):
+        vert_overline_lambda = np.zeros(lam.shape,dtype=object)
+        vert_overline_lambda[...,-1] = lam[...,-1]
+        D = np.shape(vert_overline_lambda)[-1]
+        for i in range(D-2,-1,-1):
+            vert_overline_lambda[...,i] = vert_overline_lambda[...,i+1]+lam[...,i]
+
+        return vert_overline_lambda
+
 
 
 
@@ -39,6 +95,7 @@ class Legrendrepoly:
 
 
     def multindex(self,d=None,n=None):
+        '''homogeneous polynomials of degree n, multiple indicators '''
         if n is None:
             n = self.n
         
@@ -77,7 +134,6 @@ class Legrendrepoly:
 ###################################################################
 if __name__ == '__main__':
     import numpy as np
-    Leg = Legrendrepoly(n=2,d=2)
-    Cunmber = Leg.Combination_number
-    mutidex = Leg.multindex
+    Leg = Legrendrepoly(d=2,n=3)
+    print(Leg.L_beta)
   #  print(Leg.beta)
