707e833e34b83d80e099a448af59f00bcf0e7f9b
MathChen <1538673717@qq.com>
2021-02-22 16:52:46 +0800

    完成了边界条件处理

--- fealpystudy/myfunctionspace/CRFortin3DFiniteElementSapce.py
+++ fealpystudy/myfunctionspace/CRFortin3DFiniteElementSapce.py
@@ -39,7 +39,12 @@ class CR_FortinDof():
         face2dof = np.vstack((mesh.ds.face_to_edge().T,
                                     NE+np.arange(0,NF))).T
         # face2dof.shape = (NF, 4)
-
+        '''
+        face = mesh.entity('face')
+        edge = mesh.entity('edge')
+        face_to_edge = mesh.ds.face_to_edge()
+        print(face[1,:],edge[4,:],face_to_edge[1,:])
+        '''
         return face2dof[index]
 
     def edge_to_dof(self, index=np.s_[:]):
@@ -59,7 +64,7 @@ class CR_FortinDof():
         # ipoints.shape = (gdof,3)
         return ipoints
 
-    def newinterpolation_points(self):
+    def Forininterpolation_points(self):
         mesh = self.mesh
         ipoints = np.vstack((mesh.entity_barycenter('edge'),
                                 mesh.entity('node')))
@@ -126,11 +131,8 @@ class CRFortin3DFiniteElementSpace():
     def interpolation_points(self):
         return self.dof.interpolation_points()
 
-    def newinterpolation_points(self):
-        return self.dof.newinterpolation_points()
-
-    def cell_to_dof(self, index=np.s_[:]):
-        return self.dof.cell_to_dof(index=index)
+    def Forininterpolation_points(self):
+        return self.dof.Forininterpolation_points()
 
     def cell_to_dof(self, index=np.s_[:]):
         return self.dof.cell_to_dof(index=index)
@@ -151,6 +153,24 @@ class CRFortin3DFiniteElementSpace():
         return self.TD
 
     
+
+    @barycentric
+    def face_basis(self, bc):
+        ''' basis on a face '''
+
+        phif = 2-3*np.sum(bc**2,-1) #(NQ,)
+        phie = 4*bc[...,[1,2,0]]*bc[...,[2,0,1]] #(NQ,3)
+
+        if len(bc.shape)==1:
+            phi = np.concatenate([phie,[phif]])
+        else:
+            phi = np.vstack((phie.T,phif.T)).T        
+        
+        return phi[...,None,:] #(1,4) or (NQ,1,4)
+
+
+
+
     @barycentric
     def basis(self, bc):
         """
@@ -166,10 +186,11 @@ class CRFortin3DFiniteElementSpace():
             the shape of 'phi' can be `(1, ldof)` or `(NQ, 1, ldof)`
         """
         phie = 4*bc[...,[0,0,0,1,1,2]]*bc[...,[1,2,3,2,3,3]] #(NQ,6)
-        '''
+
         phif = 2*(1-bc)**2-3*(bc[...,[1,2,3,0]]**2+bc[...,[2,3,0,1]]**2+bc[...,[3,0,1,2]]**2) #(NQ,4)
-        '''
-        phif = 10*bc**2 - 8*bc + 1 #(NQ, 4)
+     
+        #phif = 10*bc**2 - 8*bc + 1 #(NQ, 4)
+    
         phic = 2-4*np.sum(bc**2,-1) #(NQ,1)
         if len(bc.shape)==1:
             phi = np.concatenate([phie,phif,[phic]])
@@ -199,13 +220,13 @@ class CRFortin3DFiniteElementSpace():
         grad_lambda = mesh.grad_lambda()[index] #(NC, TD+1, GD)
         gphie = 4*(bc[...,None,[0,0,0,1,1,2],None]*grad_lambda[...,[1,2,3,2,3,3],:]
                     +bc[...,None,[1,2,3,2,3,3],None]*grad_lambda[...,[0,0,0,1,1,2],:])
-        '''
+
         gphif = 4*(bc[...,None,:,None]-1)*grad_lambda - 6*(bc[...,None,[1,2,3,0],None]*grad_lambda[...,[1,2,3,0],:]
                                 + bc[...,None,[2,3,0,1],None]*grad_lambda[...,[2,3,0,1],:]
                                 +bc[...,None,[3,0,1,2],None]*grad_lambda[...,[3,0,1,2],:])
-        '''
-        gphif = 20*bc[...,None,:,None]*grad_lambda - 8*grad_lambda
-
+        
+        #gphif = 20*bc[...,None,:,None]*grad_lambda - 8*grad_lambda
+        
         gphic = -8*np.sum(bc[...,None,:,None]*grad_lambda,-2)
         gphic = gphic[...,None,:]
 
@@ -297,8 +318,15 @@ class CRFortin3DFiniteElementSpace():
         uI = u(ipoints)
         return self.function(dim=dim, array=uI)
 
-    def newinterpolation(self, u, dim=None):
-        ipoints = self.dof.newinterpolation_points() #(NE+NN,3)
+
+
+    def Forininterpolation(self, u, dim=None):
+        uI = self.Forininterpolationarray(u,dim=dim)
+        return self.function(dim=dim, array=uI)
+
+
+    def Forininterpolationarray(self,u,dim=None,index=np.s_[:]):
+        ipoints = self.dof.Forininterpolation_points() #(NE+NN,3)
         uo = u(ipoints) #(NE+NN,)
 
         mesh = self.mesh
@@ -307,20 +335,20 @@ class CRFortin3DFiniteElementSpace():
         NF = mesh.number_of_faces()
         NC = mesh.number_of_cells()
 
-        uE = uo[:NE][mesh.ds.cell_to_edge()] #(NC,6)
-        uN = uo[NE:][mesh.entity('cell')]    #(NC,4)
+        uE = uo[:NE] #(NE,)     
+        uN = uo[NE:] #(NN,)
 
-        uE = uE + 0.25*(uN[:,[0,0,0,1,1,2]]+uN[:,[1,2,3,2,3,3]])-0.5*(uN[:,[2,1,1,0,0,0]]+uN[:,[3,3,2,3,2,1]]) #(NC,6)
-        uF = 0.5*uN #(NC,4)
-        uC = 0.25*np.sum(uN,-1) #(NC,1)
+        u_cell_node = uN[mesh.entity('cell')]    #(NC,4)
+        u_face_node = uN[mesh.entity('face')]    #(NF,3)
+        u_edge_node = uN[mesh.entity('edge')]    #(NE,2)
 
-        uI = np.zeros(NE+NF+NC, dtype=self.ftype)
-        cell2dof = self.cell_to_dof()
-        np.add.at(uI, cell2dof, np.vstack((uE.T,uF.T,uC.T)).T) #(gdof,)
-       # print(self.mesh.entity('node'))
-        #print(uI.shape)
-        
-        return self.function(dim=dim, array=uI)
+        uE = uE + 0.75*np.sum(u_edge_node,-1) #(NE,)
+        uF = -np.sum(u_face_node,-1) #(NF,)
+        uC = np.sum(u_cell_node,-1) #(NC,)
+
+        uI = np.hstack((uE,uF,uC))
+
+        return uI[index]
         
 
     def function(self, dim=None, array=None):
@@ -337,66 +365,111 @@ class CRFortin3DFiniteElementSpace():
             shape = (gdof, ) + dim
         return np.zeros(shape, dtype=self.ftype)
 
-    def set_dirichlet_bc(self, uh, g, threshold=None):
-        ipoints = self.interpolation_points()
-        isBdDof = self.is_boundary_dof(threshold=threshold)  
-        mesh = self.mesh  
-
-        #uh[isBdDof] = g(ipoints[isBdDof])    need to do some work
-        #idex = np.where(isBdDof)
-        #uh[idex[0]] = 0
-
-        # to get I_hu, such that \int_F (I_hu-u)p d\sigma=0 , p\in P_1(F)
-        # where F is boundary face
-
-        face2dof = self.face_to_dof() #(NF, 4)
-        face = mesh.entity('face') #(NF, 3)
-        node = mesh.entity('node') #(NE. 3)
-        NE = mesh.number_of_edges()
-
-        index = mesh.ds.boundary_face_index()
-        bc = mesh.entity_barycenter('face', index=index)
-        flag = threshold(bc)
-        index = index[flag]
-
-        bdface2dof = face2dof[index,:] #(bdNF, 4)
-        bdface = face[index,:]
+    def integral_basis(self):
+        """
+        auxiliary pure neumann boundary
+        """
+        cell2dof = self.cell_to_dof()
+        bcs, ws =self.integrator.get_quadrature_points_and_weights()
+        phi = self.basis(bcs)
+        cc = np.einsum('m, mik, i->ik', ws, phi, self.cellmeasure)
         gdof = self.number_of_global_dofs()
-        bdNF = index.shape[0]
-        Abd = np.array([
-            [2.0, 2.0, 1.0, 5.0],
-            [2.0, 1.0, 2.0, 5.0],
-            [1.0, 2.0, 2.0, 5.0]], dtype=self.ftype)
+        c = np.zeros(gdof, dtype=self.ftype)
+        np.add.at(c, cell2dof, cc)
+        return c
 
-        faceinter = mesh.integrator(3,'face')
-        bcs, ws = faceinter.get_quadrature_points_and_weights() #bcs (NQ, 3)
-        
-        bc = np.einsum('ijk,lij->lik', node[bdface,...],bcs[...,None,:]) #(NQ,bdNF,3)
-        uo = g(bc) #(NQ,bdNF)
-        b = uo[...,None]*bcs[...,None,:] #(NQ,bdNF,3)
-        bb = np.einsum('i,ijk->jk',ws,b) #(bdNF,3)
-        b = 15*np.ravel(bb,order='F')
-        I = np.arange(3*bdNF,dtype=self.itype).reshape(3,bdNF)
-        
-        shape = (3, bdNF, 4)
-        J = np.broadcast_to(bdface2dof[None,:,:],shape = shape)
-        Abd = np.broadcast_to(Abd[:,None,:],shape = shape)
-        I = np.broadcast_to(I[:,:,None],shape = shape)
 
-        Abd = csr_matrix((Abd.flat, (I.flat, J.flat)), shape=(3*bdNF, gdof)).todense()
-        Abd = Abd[:,isBdDof]
-        bdgdof = Abd.shape[1]
-        idex = np.array(np.where(isBdDof)).squeeze()
-        #uh[idex[1:]] = spsolve(Abd[1:bdgdof,1:bdgdof],b[1:bdgdof])
-        
-
-    
-
-        #print(uh[idex[1:]].shape, spsolve(Abd[1:bdgdof,1:bdgdof],b[1:bdgdof]).shape)
-
-        
+    def set_dirichlet_bc(self, uh, g, threshold=None):
+        ipoints = self.Forininterpolation_points()
+        isBdDof = self.is_boundary_dof(threshold=threshold)  
+        uh[isBdDof] = self.Forininterpolationarray(g,dim=None,index=isBdDof)
+       # print(isBdDof.shape)       
 
         return isBdDof
+
+
+    def set_neumann_bc(self, F, gN, threshold=None, q=None):
+        '''
+        设置 Neumann 边界条件到载荷矩阵 F 中
+        '''
+
+        mesh = self.mesh
+
+        dim = 1 if len(F.shape)==1 else F.shae[1]
+
+        if type(threshold) is np.ndarray:
+            index = threshold
+        else:
+            index = self.mesh.ds.boundary_face_index()
+            if threshold is not None:
+                bc = self.mesh.entity_barycenter('face',index=index)
+                flag = threshold(bc)
+                index = index[flag]
+
+        face2dof = self.face_to_dof()[index]
+        n = mesh.face_unit_normal(index=index)
+        measure = mesh.entity_measure('face', index=index)
+
+        qf = self.integralalg.faceintegrator if q is None else mesh.integrator(q, 'face')
+        bcs, ws = qf.get_quadrature_points_and_weights()
+        phi = self.face_basis(bcs)
+
+        pp = mesh.bc_to_point(bcs, etype='face', index=index)
+        val = gN(pp, n) #(NQ, NF, ...), 假设 gN 是一个函数
+
+        bb = np.einsum('m, mi..., mik, i->ik...', ws, val, phi, measure)
+        if dim == 1:
+            np.add.at(F, face2dof, bb)
+        else:
+            np.add.at(F, (face2dof, np.s_[:]), bb)
+
+
+
+    def set_robin_bc(self, A, F, gR, threshold=None, q=None):
+        """
+        设置 Robin 边界条件到离散系统 Ax = b 中.
+        """
+        mesh = self.mesh
+        dim = 1 if len(F.shape) == 1 else F.shape[1]
+
+        if type(threshold) is np.ndarray:
+            index = threshold
+        else:
+            index = self.mesh.ds.boundary_face_index()
+            if threshold is not None:
+                bc = self.mesh.entity_barycenter('face', index=index)
+                flag = threshold(bc)
+                index = index[flag]
+
+        face2dof = self.face_to_dof()[index]
+        print(index)
+        qf = self.integralalg.faceintegrator if q is None else mesh.integrator(q, 'face')
+        bcs, ws = qf.get_quadrature_points_and_weights()
+
+        measure = mesh.entity_measure('face', index=index)
+
+        phi = self.face_basis(bcs)
+        pp = mesh.bc_to_point(bcs, etype='face', index=index)
+        n = mesh.face_unit_normal(index=index)
+
+        val, kappa = gR(pp, n) #(NQ, NF, ...)
+
+        bb = np.einsum('m, mi..., mik, i->ik...', ws, val, phi, measure)
+        if dim == 1:
+            np.add.at(F, face2dof, bb)
+        else:
+            np.add.ta(F, (face2dof, np.s_[:]), bb)
+
+        FM = np.einsum('m, mi, mij, mik, i->ijk', ws, kappa, phi, phi, measure)
+
+        I = np.broadcast_to(face2dof[:, :, None], shape=FM.shape)
+        J = np.broadcast_to(face2dof[:, None, :], shape=FM.shape)
+
+        A += csr_matrix((FM.flat, (I.flat, J.flat)), shape=A.shape)
+
+        return A, F       
+
+
         
 
 
@@ -423,19 +496,20 @@ if __name__ == '__main__':
 
     #general pde model
     x, y, z = sp.symbols('x0,x1,x2')
-    #u = sp.sin(sp.pi*x)*sp.sin(sp.pi*y)*sp.sin(sp.pi*z)
-    u = sp.sin(sp.pi*x)*sp.exp(x+y+z)*y*(1-y)*z*(1-z)
-    #u = 1-x-y-z
-
+    #u = sp.sin(sp.pi*x)*sp.sin(sp.pi*y)*sp.sin(sp.pi*z)-(2/sp.pi)**3
+    u = sp.sin(2*sp.pi*x)*sp.sin(2*sp.pi*y)*sp.sin(2*sp.pi*z)
+    #u = sp.sin(sp.pi*x)*sp.exp(x+y+z)
+    #u = sp.cos(sp.pi*x)*sp.cos(sp.pi*y)*sp.cos(sp.pi*z)
+    #u = (1-x-y-z)**5
     pde = generalelliptic3D(u,x,y,z,
-          Dirichletbd='(x==1.0)|(x==0.0)|(y==0.0)|(y==1.0)|(z==0)|(z==1)')
+          Dirichletbd='(x==1.0)|(x==0.0)|(y==1)|(y==0.0)',
+          Neumannbd= '(z==0)|(z==1)')
+    '''
+    pde = generalelliptic3D(u,x,y,z,
+          Neumannbd='(x==1.0)|(x==0.0)|(y==0.0)|(y==1)|(z==0)|(z==1)')
+    '''
 
-    #load mesh
-    domain = pde.domain()
     mf = MeshFactory()
-    mesh = mf.boxmesh3d(domain, nx=1,ny=1,nz=1,meshtype='tet')
-    #mesh = mf.one_tetrahedron_mesh()
-
     '''
     node = np.array([
                 [0.0, 0.0, 0.0],
@@ -447,16 +521,19 @@ if __name__ == '__main__':
                 [0, 1, 2, 3],
                 [0, 1, 2, 4]], dtype=np.int_)
     mesh = TetrahedronMesh(node, cell)
+
+    #mesh = mf.one_tetrahedron_mesh()
+
     
     spaceCRFortion = CRFortin3DFiniteElementSpace(mesh)
     spaceLagrange = LagrangeFiniteElementSpace(mesh,p=2)
    
    
-    uICRFortin = spaceCRFortion.newinterpolation(pde.solution)
+    uICRFortin = spaceCRFortion.Forininterpolation(pde.solution)
     uILP2 = spaceLagrange.interpolation(pde.solution)
     exactu = sp.lambdify((x,y,z),u,'numpy')
 
-    bc = np.array([1,0,0,0],dtype=mesh.ftype)
+    bc = np.array([0.5,0.2,0.2,0.1],dtype=mesh.ftype)
     bcs = mesh.bc_to_point(bc)
 
     print('interpoints=', bcs.squeeze(),
@@ -466,7 +543,13 @@ if __name__ == '__main__':
 
     '''
 
-  
+
+
+   #load mesh
+    domain = pde.domain()
+    mesh = mf.boxmesh3d(domain, nx=1,ny=1,nz=1,meshtype='tet')
+
+
 
     NDof = np.zeros(4, dtype=mesh.itype)
     errormatrix = np.zeros((2,4),dtype = mesh.ftype)
@@ -475,27 +558,39 @@ if __name__ == '__main__':
 
     for i in range(4):
         print('Step:', i)
-        space = CRFortin3DFiniteElementSpace(mesh)
-
-        #print(space.newinterpolation_points().shape, mesh.number_of_edges()+mesh.number_of_nodes())
-   
+        space = CRFortin3DFiniteElementSpace(mesh)   
         NDof[i] = space.number_of_global_dofs()
        
         uh = space.function()
         A = space.stiff_matrix()
         F = space.source_vector(pde.source)
+        
+        bc = bdc.NeumannBC(space, pde.neumann, threshold=pde.is_neumann_boundary)
+        #A, F = bc.apply(F,A=A)
+        # uh[:] = spsolve(A, F)[:-1] # pure neumann
 
+        F = bc.apply(F)
+
+        bc = bdc.RobinBC(space, pde.robin, threshold=pde.is_robin_boundary)
+
+        A, F = bc.apply(A,F)
+
+ 
         bc = bdc.DirichletBC(space, pde.dirichlet, threshold=pde.is_dirichlet_boundary)
         A, F = bc.apply(A,F,uh)
 
         uh[:] = spsolve(A, F)
 
-    
+      
+
+       # space.set_neumann_bc(F,pde.neumann,threshold=pde.is_neumann_boundary)
+
+
+
 
         #插值误差
         #uI = space.interpolation(pde.solution) #插值点给法有问题
-        #uI = space.newinterpolation(pde.solution) #重构P2插值不行, 该插值点的值会通过面影响到其他单元，故不行
-
+        uI = space.Forininterpolation(pde.solution) # 重构建了P2插值，完美
         errormatrix[0, i] = space.integralalg.L2_error(pde.solution,  uh.value)
         errormatrix[1, i] = space.integralalg.L2_error(pde.gradient,  uh.grad_value)
 
@@ -507,10 +602,20 @@ if __name__ == '__main__':
     show_error_table(NDof, errortype,  errormatrix)
 
     #plt.show()
+ 
 
 
 
-    '''
+
+
+
+
+
+
+
+
+
+'''
     #check the dof and interpolation point
     dof = CR_FortinDof(mesh)
     ipoints = dof.interpolation_points()
@@ -542,7 +647,7 @@ if __name__ == '__main__':
 
    # print(bcs.shape,cell2dof.shape,uh.shape,uhs.shape,guhs.shape)
 
-    '''
+'''
 
  
 
@@ -557,41 +662,40 @@ if __name__ == '__main__':
 
 
 
-'''
 
 
 
 
-
-
-   
-    node = mesh.entity('node')
-    cell = mesh.entity('cell')
-    face = mesh.entity('face')
-    edge = mesh.entity('edge')
-
-    cell2face = mesh.ds.cell_to_face()
-    cell2edge = mesh.ds.cell_to_edge()
-
-    #print(cell2edge,'\n\n', edge)
-
-    #print(cell2dof)
-
-    fig = plt.figure()
-    axes = Axes3D(fig)
-    mesh.add_plot(axes)
-
-
-    #mesh.find_node(axes, showindex=True, color='b', fontsize=30)
-    #mesh.find_edge(axes, showindex=True, color='g', fontsize=24)
-    mesh.find_face(axes, showindex=True, fontsize=22)
-    #mesh.find_cell(axes, showindex=True, fontsize=20)
-    ipoints = spaceCRFortion.interpolation_points()
-    mesh.find_node(axes, showindex=True, color='b', fontsize=2)
-    #mesh.find_node(axes, node=ipoints, showindex=True, color='r', fontsize=30)
-    plt.show()
 
 '''
 
 
+node = mesh.entity('node')
+cell = mesh.entity('cell')
+face = mesh.entity('face')
+edge = mesh.entity('edge')
+
+cell2face = mesh.ds.cell_to_face()
+cell2edge = mesh.ds.cell_to_edge()
+
+#print(cell2edge,'\n\n', edge)
+
+#print(cell2dof)
+
+fig = plt.figure()
+axes = Axes3D(fig)
+mesh.add_plot(axes)
+
+
+#mesh.find_node(axes, showindex=True, color='b', fontsize=30)
+#mesh.find_edge(axes, showindex=True, color='g', fontsize=24)
+#mesh.find_face(axes, showindex=True, fontsize=22)
+#mesh.find_cell(axes, showindex=True, fontsize=20)
+ipoints = spaceCRFortion.interpolation_points()
+#mesh.find_node(axes, showindex=True, color='b', fontsize=2)
+mesh.find_node(axes, node=ipoints, showindex=True, color='r', fontsize=30)
+plt.show()
+
+
+'''
 #在要求边界P1投影为0下，只能解齐次dirichlet,非齐次的不能解, 因为边界投影自由度不够
